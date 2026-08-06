55a606c7779b9e79143f5f3e72f5a8d27294c16c
umerisanhacker <140935196+umerisanhacker@users.noreply.github.com>
2026-08-06 11:22:41 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const path = require('path');
 ffmpeg.setFfmpegPath(ffmpegInstaller.path);
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
